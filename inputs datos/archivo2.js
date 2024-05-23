93261c3b8bd8da2acbaf38e5fc47d11320ad92f3
MatiassOrellana <matias.orellana@alumnos.ucn.cl>
2024-05-23 17:21:02 -0400

    feat: added inputs

--- inputs datos/archivo2.js
+++ inputs datos/archivo2.js
@@ -2,6 +2,10 @@ let nombre, edad; //variables declaradas
 nombre = prompt('Ingrese su nombre');
 edad = prompt('Ingrese su edad');
 
+document.write("nombre ", nombre);
+document.write("<br>");
+document.write("edad ", edad);
+
 //con // se comenta para entender mejor el codigo
 
 
