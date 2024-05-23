2fbbdef8da59ac957296e6753f01893df10d3716
MatiassOrellana <matias.orellana@alumnos.ucn.cl>
2024-05-23 17:39:56 -0400

    feat: added operations

--- inputs datos/archivo2.js
+++ inputs datos/archivo2.js
@@ -1,10 +1,37 @@
-let nombre, edad; //variables declaradas
+let nombre, edad, a, b, sum, res, mult, div, pot, resid; //variables declaradas
 nombre = prompt('Ingrese su nombre');
 edad = prompt('Ingrese su edad');
+a = prompt('primer numero');
+b = prompt('segundo numero');
+
+sum = a + b;
+res = a - b;
+mult = a * b;
+div = parseFloat(a/b);
+pot = a ** b;
+resid = a % b;
+
+const numerofavorito = 8;
 
 document.write("nombre ", nombre);
 document.write("<br>");
 document.write("edad ", edad);
+document.write("<br>");
+
+document.write("el numero favorito es: ", numerofavorito , " de pool");
+document.write("<br>");
+document.write("resultado suma: ", sum);
+document.write("<br>");
+document.write("resultado resta: ", res);
+document.write("<br>");
+document.write("resultado multiplicacion: ", mult);
+document.write("<br>");
+document.write("resultado division: ", div);
+document.write("<br>");
+document.write("resultado potencia: ", pot);
+document.write("<br>");
+document.write("resultado raiz: ", resid);
+
 
 //con // se comenta para entender mejor el codigo
 
