12a2987df6492f55d2943862606f3d71eb8bcb2d
MatiassOrellana <matias.orellana@alumnos.ucn.cl>
2024-05-23 15:46:05 -0400

    feat: added encapsulation in let variable

--- tipos de datos/archivo.js
+++ tipos de datos/archivo.js
@@ -3,11 +3,14 @@ var nombre ="Matias";
 
 let cantidad = 25;
 let precio = "N/A"; 
+let persona = {nombre: "Diego", edad: 24}
 
 document.write(nombre);
 document.write("<br>");
 document.write(cantidad);
 document.write("<br>");
 document.write(precio);
+
+document.write(persona.nombre);
 
 
